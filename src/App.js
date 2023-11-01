d8a5610175228d053927e2a8db728f2b79f037ee
Andrew <andrewmreiblein013@gmail.com>
2023-11-01 13:50:13 +0200

    Adding waiting page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import myImage from "./assets/Logo.svg";
+import facebookIcon from "./assets/FB.svg";
+import twitterIcon from "./assets/IG.svg";
+import linkedinIcon from "./assets/email.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="app-background">
+      <img src={myImage} alt="My Logo" className="app-logo" />
+
+      <div className="social-links">
+        <a href="https://www.facebook.com">
+          <img src={facebookIcon} alt="Facebook" />
         </a>
-      </header>
+        <a href="https://www.twitter.com">
+          <img src={twitterIcon} alt="Twitter" />
+        </a>
+        <a href="https://www.linkedin.com">
+          <img src={linkedinIcon} alt="LinkedIn" />
+        </a>
+      </div>
+      <div className="centered-svg"></div>
+      <div className="rights">
+        "Copyright © 2023 All About Adventure. All Rights Reserved."
+      </div>
     </div>
   );
 }
